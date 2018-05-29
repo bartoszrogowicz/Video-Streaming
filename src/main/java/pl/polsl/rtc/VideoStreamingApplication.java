5ac72c8e649c8ec3659d51e6e1f38e3cdec2147f
Bartosz Rogowicz <bartosz.rogowicz@gmail.com>
2018-05-29 17:16:29 +0200

    Init project

---
+++ src/main/java/pl/polsl/rtc/VideoStreamingApplication.java
@@ -0,0 +1,12 @@
+package pl.polsl.rtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoStreamingApplication.class, args);
+    }
+}
